9415156f397fc1ab4e50b775d10907831d95edbb
dluzong <daphneluzong@gmail.com>
2024-11-10 03:21:27 -0500

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Daphne Luzong on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
